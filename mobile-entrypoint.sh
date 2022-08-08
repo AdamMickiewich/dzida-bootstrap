829e6c1cf876f0fadffda4fa6d04418702fa70eb
Adam Mickiewich <110844207+AdamMickiewich@users.noreply.github.com>
2022-08-08 18:10:00 +0000

    Update install script url

--- mobile-entrypoint.sh
+++ mobile-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 while :; do
-  curl "https://raw.githubusercontent.com/VolyaTeam/db1000n/main/install.sh" | bash
+  curl "https://raw.githubusercontent.com/arriven/db1000n/main/install.sh" | bash
   ./db1000n
 done

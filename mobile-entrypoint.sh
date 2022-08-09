68984472089c0b6fcb96252814ed54638dd01bc7
Adam Mickiewich <110844207+AdamMickiewich@users.noreply.github.com>
2022-08-09 07:37:46 +0000

    Update mobile entrypoint

--- mobile-entrypoint.sh
+++ mobile-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+pkg install proot resolv-conf
+
 while :; do
   curl "https://raw.githubusercontent.com/arriven/db1000n/main/install.sh" | bash
-  ./db1000n
+  proot -b $PREFIX/etc/resolv.conf:/etc/resolv.conf ./db1000n
 done

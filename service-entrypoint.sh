eb054372869a793e0df0c8207c536e53946e1902
Adam Mickiewich <110844207+AdamMickiewich@users.noreply.github.com>
2022-08-11 07:18:35 +0000

    Update service-entrypoint.sh

--- service-entrypoint.sh
+++ service-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 while :; do
-  curl "https://raw.githubusercontent.com/arriven/db1000n/main/install.sh" | bash
+  curl "https://raw.githubusercontent.com/arriven/db1000n/main/install.sh" | bash -x
   ./db1000n
 done
